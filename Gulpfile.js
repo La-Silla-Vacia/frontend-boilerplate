e23e2c77189f2eae04fa354e63a7ef2041b6fff8
Wietse Neven <info@wn-multimedia.nl>
2017-02-09 15:11:38 -0500

    more configurations

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,6 +10,7 @@ const browserSync = require('browser-sync').create();
 const htmlLint = require('gulp-html-lint');
 const sassLint = require('gulp-sass-lint');
 const eslint = require('gulp-eslint');
+const ghPages = require('gulp-gh-pages');
 
 gulp.task('run', () => {
   const bundler = browserify('./src/js/app.js', {debug:true})
@@ -22,7 +23,7 @@ gulp.task('run', () => {
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('public/js'));
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('lint', () => {
@@ -44,7 +45,7 @@ gulp.task('sass', () => {
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('html-lint', function() {
@@ -52,22 +53,27 @@ gulp.task('html-lint', function() {
     .pipe(htmlLint())
     .pipe(htmlLint.format())
     .pipe(htmlLint.failOnError())
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('browser-sync', () => {
   browserSync.init({
     server: {
-      baseDir: "./public/"
+      baseDir: "./dist/"
     }
   });
+});
+
+gulp.task('deploy', ['html-lint', 'images', 'run', 'sass'], function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
 
 gulp.task('watch', () => {
   gulp.watch('./src/js/**/*.*', ['lint', 'run']);
   gulp.watch('./src/sass/**/*.scss', ['sass']);
   gulp.watch('./src/**/*.html', ['html-lint']);
-  gulp.watch(['./public/**/*.*','./css/**/*.*','./public/*.html']).on('change', browserSync.reload);
+  gulp.watch(['./dist/**/*.*','./css/**/*.*','./dist/*.html']).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['watch', 'lint', 'html-lint', 'run', 'sass', 'browser-sync']);
